1c3c54cdb96b0cd616eba020c138b44c8c1d3d25
David Lin <>
2024-03-14 20:48:20 +1100

    Add anatomy example

--- examples/common.js
+++ examples/common.js
@@ -1,6 +1,6 @@
 // reset example survey upon complete button clicked
 window.addEventListener('surveychange', (e) => {
-  if (e.detail.name === 'COMPLETE_OK') {
+  if (e.detail.name === 'COMPLETE_OK' || e.detail.name === 'SURVEY_CLOSE') {
     e.target.api.reset();
   }
 });
