7721154f3e3f528172b9f6c9741654dbf8555ff7
David Lin <>
2024-07-24 20:51:41 +1000

    allow closing page upon finish/close

--- examples/common.js
+++ examples/common.js
@@ -1,6 +1,11 @@
 // reset example survey upon complete button clicked
 window.addEventListener('surveychange', (e) => {
   if (e.detail.name === 'COMPLETE_OK' || e.detail.name === 'SURVEY_CLOSE') {
-    e.target.api.reset();
+    var searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has('close')) {
+      window.close();
+    } else {
+      e.target.api.reset();
+    }
   }
 });
