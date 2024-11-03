813de798cb99b7b9e9cd10b7c4782a16134672c3
David Lin <davidforest@gmail.com>
2024-11-03 12:27:49 +1100

    Update after survey tag update on reset after complete

--- examples/common.js
+++ examples/common.js
@@ -1,11 +1,10 @@
 // reset example survey upon complete button clicked
 window.addEventListener('surveychange', (e) => {
-  if (e.detail.name === 'COMPLETE_OK' || e.detail.name === 'SURVEY_CLOSE') {
+  if (e.detail.name === 'SURVEY_CLOSE') {
     var searchParams = new URLSearchParams(window.location.search);
     if (searchParams.has('close')) {
       window.close();
-    } else {
-      e.target.api.reset();
     }
+    e.target.api.reset();
   }
 });
